04cad0117fdcd114ec3793dd3dbb4a8dd3ea5065
Vaaleri26 <93161063+Vaaleri26@users.noreply.github.com>
2022-02-23 19:37:29 +0300

    Update csv.js

--- csv.js
+++ csv.js
@@ -3,7 +3,10 @@ const http = require("http");
 const host = 'localhost';
 const port = 8000;
 
-const requestListener = function (req, res) {};
+const requestListener = function (req, res) {
+	res.setHeader("Content-Type", "text/csv");
+	res.setHeader("Content-Disposition", "attachment;filename=oceanpals.csv");//способ отображения данных
+	res.writeHead(200);res.end(`id,name,email\n1,Sammy Shark,shark@ocean.com`);};//вызов содержит строку
 
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
