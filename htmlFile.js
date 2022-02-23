8e82ddf5f12ecb3600d4d410e8d875de9b4675ae
Vaaleri26 <93161063+Vaaleri26@users.noreply.github.com>
2022-02-23 19:42:44 +0300

    Update htmlFile.js

--- htmlFile.js
+++ htmlFile.js
@@ -1,9 +1,24 @@
 const http = require("http");
+const fs = require('fs').promises;
+
 
 const host = 'localhost';
 const port = 8000;
 
-const requestListener = function (req, res) {};
+const requestListener = function (req, res) {
+s.readFile(__dirname + "/index.html")//fs.readFile() для загрузки файла,__dirname путь,/index.html, для загрузки данного файла
+	.then(contents => {//then() возвращает данные при успещном выполнении ,contents содержит эти самые данные
+		res.setHeader("Content-Type", "text/html");
+		res.writeHead(200);
+		res.end(contents);// отправляем на клиент данные
+	}
+)
+.catch(err => {// при ошибке 
+	res.writeHead(500);
+	res.end(err);
+	return;
+}
+       };
 
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
